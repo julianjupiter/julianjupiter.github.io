e535ae21775f133ab00fa80964b8fc121e7586b8
Julian Jupiter <julianjupiter.io@gmail.com>
2023-03-13 23:40:45 +0800

    Update greetings

--- src/consts.ts
+++ src/consts.ts
@@ -4,7 +4,7 @@ export const SITE_TITLE = "Julian Jupiter";
 export const SITE_KEYWORDS  ="blog, programming, Java, Spring, Spring Boot, Linux, SQL, JavaScript, HTML, CSS, Web development";
 export const SITE_AUTHOR = "Julian Jupiter";
 
-export const GREETINGS = "Hi! My name is Julian Jupiter.";
+export const GREETINGS = "Hi, I'm Julian Jupiter!";
 export const STATEMENT = "I'm a passionate software developer from the Philippines. I have several years of experience in backend and frontend development using technologies such as Java, Linux, SQL, JavaScript and a lot more.";
 
 export const DISQUS = "julianjupitergithubio";
