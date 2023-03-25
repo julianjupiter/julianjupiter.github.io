e75a6d409a42d92175ef2587caf0eae741e3e9d9
Julian Jupiter <julianjupiter.io@gmail.com>
2023-03-25 19:06:09 +0800

    Add more constants

--- src/consts.ts
+++ src/consts.ts
@@ -3,6 +3,8 @@ export const SITE_URL = "https://julianjupiter.com";
 export const SITE_TITLE = "Julian Jupiter";
 export const SITE_KEYWORDS  ="blog, programming, Java, Spring, Spring Boot, Linux, SQL, JavaScript, HTML, CSS, Web development";
 export const SITE_AUTHOR = "Julian Jupiter";
+export const SITE_FAVICON = "/assets/images/favicon.png";
+export const SITE_COVER = "/assets/images/cover.png";
 
 export const GREETINGS = "Hi, I'm Julian Jupiter!";
 export const STATEMENT = "I'm a passionate software developer from the Philippines. I have several years of experience in backend and frontend development using technologies such as Java, Linux, SQL, JavaScript and a lot more.";
@@ -14,6 +16,7 @@ export const PROFILE_PHOTO = "/assets/images/profile.jpg";
 export const DISQUS = "julianjupitergithubio";
 export const GITHUB_URL = "https://github.com/julianjupiter";
 export const LINKEDIN_URL = "https://linkedin.com/in/julianjupiter";
-export const TWITTER_URL = "https://twitter.com/julianvjupiter";
+export const TWITTER_USERNAME = "@julianvjupiter";
+export const TWITTER_URL = `https://twitter.com/${TWITTER_USERNAME}`;
 export const REDDIT_URL = "https://reddit.com/user/jvjupiter";
 export const QUORA_URL = "https://www.quora.com/profile/Julian-Jupiter";
